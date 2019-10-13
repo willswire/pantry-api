74a1e9d687e12846ac9cecca85275d1dedddc34b
Will Walker <wwalker@udel.edu>
2019-10-13 16:49:11 -0400

    add mongodb implementation

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 
 // init project
 const express = require("express");
+const mongo = require('mongodb');
 const app = express();
+
+// connect to the mongodb instance in our atlas org account
+const MongoClient = mongo.MongoClient;
+const uri = "mongodb+srv://dbAdmin:ih8python@cluster0-jedvy.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("connection to db established!")
+    client.close();
+});
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
