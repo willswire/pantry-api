72a7d1be004f180e3bb1367533c853b65548de24
Scott Benton <scott.ma.benton@gmail.com>
2019-10-18 14:05:46 -0400

    Removed unused index file, moved database connection information to a new file in src

---
+++ src/database.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb+srv://dbAdmin:ih8python@cluster0-jedvy.mongodb.net/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+client.connect(err => {
+  const collection = client.db('test').collection('devices');
+  console.log('connection to db established');
+  client.close();
+});
