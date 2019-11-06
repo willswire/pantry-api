74419157751218cf3e8f0b532d0fba49f0159876
Connor Onweller <onweller@udel.edu>
2019-11-05 21:59:38 -0500

    Fixed get users

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -5,8 +5,8 @@ const users = Router();
 const controller = new UserController();
 
 users.post('/', controller.createUser);
-users.get('/', controller.getUser);
-users.get('/:userID', controller.getUsers);
+users.get('/', controller.getUsers);
+users.get('/:userID', controller.getUser);
 users.get('/:userID/lists', controller.getUserLists);
 users.put('/:userID', controller.updateUser);
 users.delete('/:userID', controller.deleteUser);
